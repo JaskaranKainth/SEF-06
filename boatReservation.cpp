8ef7bbfecb280a7d6c7e3c7d317edc577df2916c
JaskaranKainth <Jaskarankainth@hotmail.com>
2022-04-21 19:42:38 -0400

    Menu creation

---
+++ boatReservation.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+
+
+int main(){
+// user input options                                                        
+	const int length = 1, width = 2, electrical = 3, recommendation = 4; 
+//user input
+	int choice;
+
+
+	cout << "Welcome to the Marina " << endl;
+		cout << "________________________";
+	cout << " 1 - Enter the length of the boat (feet + inches or meters) \n";
+    cout << " 2 - Enter the beam (width) of the boat (feet + inches or meters) \n";
+    cout << " 3 - Enter the electrical requirement for the boat (None, 30Amp, 50Amp) \n";
+    cout << " 4 - System Outputs the recommended Slip (dock) Assignment \n";
+    cout << " Please enter a option... \n";
+
+    cin >> choice;
+
+
+
+
+	}
