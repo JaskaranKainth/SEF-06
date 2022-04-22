c310afe4d18acd6efb3eb2002e44048b0ce2a164
JaskaranKainth <Jaskarankainth@hotmail.com>
2022-04-22 15:46:56 -0400

    conversion function

--- boatReservation.cpp
+++ boatReservation.cpp
@@ -7,6 +7,7 @@ using namespace std;
 
 
 
+
 int main(){
 //user input
 	int choice;
@@ -15,41 +16,46 @@ int main(){
 
 	unsigned long int fact;
 
-	int conversion = length
-
+	int conversion = num * 30.48;
+	int slip;
 
 
 	while(1){
 		cout << "Welcome to the Marina " << endl;
 		cout << "________________________ \n";
-	cout << " 1 - Enter the length of the boat (feet + inches or meters) \n";
-    cout << " 2 - Enter the beam (width) of the boat (feet + inches or meters) \n";
+	cout << " 1 - Enter the length of the boat (Meters) \n";
+    cout << " 2 - Enter the beam (width) of the boat (meters) \n";
     cout << " 3 - Enter the electrical requirement for the boat (None, 30Amp, 50Amp) \n";
     cout << " 4 - System Outputs the recommended Slip (dock) Assignment \n";
     cout << " Please enter a option: " ;
 
     cin >> choice;
+
+
+	if(choice == 0 || choice > 4){
+		cout << "Thats not a valid option... \n";
+		}
 	
 	switch(choice){
 		case 1:
 			cout << "Enter the length of your boat: " << endl;
 			cin >> num;
 
-			cout << "The length of the boat is: " << num << endl;
+			cout << "The length of the boat is: " << num << conversion << "cm" << endl;
 			break;
 
 		case 2:
 			cout << "Enter the width of your boat: " << endl;
 			cin >> num;
 
-			cout << "The width of the boat is: " << num << endl;
+			cout << "The width of the boat is: " << num << conversion << "cm" << endl;
 			break;
 		
 		case 3: 
 			cout << "Enter the electrical requirements of your boat: " << endl;
 			cin >> num;
 
-			cout << "The electrical requirements of the boat are: " << num << endl;
+			cout << "The electrical requirements of the boat are: " << num << "amps" << endl;
 			break;
 
 		case 4:
