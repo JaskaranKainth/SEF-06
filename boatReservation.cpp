776dfeff965732acb07538b2d81e33dcb77f16c7
JaskaranKainth <Jaskarankainth@hotmail.com>
2022-04-22 15:46:47 -0400

    Conversion function

--- boatReservation.cpp
+++ boatReservation.cpp
@@ -1,50 +1,62 @@
 #include <iostream>
 #include <iomanip>
+#include<stdio.h>
 
 using namespace std;
+
 
 
 
 int main(){
 //user input
 	int choice;
-	int length;
-	int width;
-	int electrical;
-	int output;
+	int num;
+	int i;
+
+	unsigned long int fact;
+
+	int conversion = length
 
 
-	cout << "Welcome to the Marina " << endl;
+
+	while(1){
+		cout << "Welcome to the Marina " << endl;
 		cout << "________________________ \n";
 	cout << " 1 - Enter the length of the boat (feet + inches or meters) \n";
     cout << " 2 - Enter the beam (width) of the boat (feet + inches or meters) \n";
     cout << " 3 - Enter the electrical requirement for the boat (None, 30Amp, 50Amp) \n";
     cout << " 4 - System Outputs the recommended Slip (dock) Assignment \n";
-    cout << " Please enter a option:" ;
+    cout << " Please enter a option: " ;
 
     cin >> choice;
-
-
-
-	if (choice > 0 && choice < 5)
-		cout << choice;
-	else if (choice == 5)
-		cout << " Sorry, please choose a valid option.";
-
-	// else if (choice == 1){
-	// 	cout << "Enter the length of the boat: ";
-	//	cin >> length;
-	//     }
-	// else if (choice == 2){
-    //     cout << "The width of the boat is " << choice;
-	//     }
-    // else if (choice == 3){
-    //     cout << "the Amps of the boat are " << choice;
-	//     }
-    // else if (choice += 4){
-    //     cout << "The recommended slips for your boat is " << choice;
 	
-	//     }
-    // else if (choice == 8)
-	// 	cout << " Sorry, please choose a valid option.";
+	switch(choice){
+		case 1:
+			cout << "Enter the length of your boat: " << endl;
+			cin >> num;
+
+			cout << "The length of the boat is: " << num << endl;
+			break;
+
+		case 2:
+			cout << "Enter the width of your boat: " << endl;
+			cin >> num;
+
+			cout << "The width of the boat is: " << num << endl;
+			break;
+		
+		case 3: 
+			cout << "Enter the electrical requirements of your boat: " << endl;
+			cin >> num;
+
+			cout << "The electrical requirements of the boat are: " << num << endl;
+			break;
+
+		case 4:
+        	cout << "The recommended slips for your boat is " << num;
+			exit(0);
 	}
+	}
+	cout << "Thanks for stopping by the Marina";
+	return 0;
+}
