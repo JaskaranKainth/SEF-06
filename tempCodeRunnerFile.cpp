cff856080c2a7b1fde71ef4cd7714e200fb1a4b8
JaskaranKainth <Jaskarankainth@hotmail.com>
2022-04-22 16:55:27 -0400

    Slips delcarations

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,1 +1,9 @@
-
+	// if(num == 0 || num == 30){
+			// 	cout << "The electrical requirements of the boat are regular, with a power of: " << num << " amps" << endl;
+			// } else if(num == 0 || num == 30 || num == 50){
+			// 	cout << "The electical requirements of the boat are large, with a power of: " << num << " amps"  << endl;
+			// } else if(num == 50){
+			// 	cout << "The electical requirements of the boat are upgraded, with a power of: " << num << " amps"  << endl;
+			// } else {
+			// 	cout << "something went wrong...";
+			// }
